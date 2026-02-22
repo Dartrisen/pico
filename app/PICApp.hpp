d73806bf5e291ba5c527af485125b4a675622f00
dartrisen <calculating.scientist@gmail.com>
2026-02-22 11:48:31 +0000

    reformat pic app file

--- app/PICApp.hpp
+++ app/PICApp.hpp
@@ -2,13 +2,16 @@
 #include <memory>
 #include "IEngine.hpp"
 
-class PICApp {
+class PICApp
+{
 public:
     explicit PICApp(std::unique_ptr<IEngine> engine, double dt)
         : engine_(std::move(engine)), dt_(dt) {}
 
-    void run(int nsteps) {
-        for (int i = 0; i < nsteps; ++i) {
+    void run(int nsteps)
+    {
+        for (int i = 0; i < nsteps; ++i)
+        {
             engine_->advance(dt_);
             // diagnostics, IO, checkpoints
         }
