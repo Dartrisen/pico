64ee0711a5f2d8df7582d127d6ecf8e2cbc5739b
dartrisen <calculating.scientist@gmail.com>
2026-02-22 11:48:54 +0000

    reformat yee maxwell engine file

--- engine/modules/field/YeeMaxwell.hpp
+++ engine/modules/field/YeeMaxwell.hpp
@@ -1,27 +1,27 @@
 #pragma once
 #include "kernels/field/maxwell_yee.hpp"
-#include "field/include/field_em.hpp"
+#include "data/field/include/field_em.hpp"
 
-namespace pico::modules::field {
+namespace pico::modules::field
+{
 
-template<size_t BLOCK_SIZE>
-struct YeeMaxwell {
+    template <size_t BLOCK_SIZE>
+    struct YeeMaxwell
+    {
 
-    inline void solve(
-        EMFields<BLOCK_SIZE>& fields,
-        const FieldSystem<BLOCK_SIZE>& J,
-        float dt
-    ) {
-        const Grid& grid = fields.E.grid();
+        inline void solve(
+            EMFields<BLOCK_SIZE> &fields,
+            const FieldSystem<BLOCK_SIZE> &J,
+            float dt)
+        {
+            const Grid &grid = fields.E.grid();
 
-        kernels::field::MaxwellYeeKernel<BLOCK_SIZE>::update_B(
-            fields, grid, dt
-        );
+            kernels::field::MaxwellYeeKernel<BLOCK_SIZE>::update_B(
+                fields, grid, dt);
 
-        kernels::field::MaxwellYeeKernel<BLOCK_SIZE>::update_E(
-            fields, J, grid, dt
-        );
-    }
-};
+            kernels::field::MaxwellYeeKernel<BLOCK_SIZE>::update_E(
+                fields, J, grid, dt);
+        }
+    };
 
 } // namespace pico::modules::field
