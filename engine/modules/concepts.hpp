8937833e198645098d802406b38b6da85fce55c0
dartrisen <calculating.scientist@gmail.com>
2026-02-22 11:50:01 +0000

    add correct pusher concept

--- engine/modules/concepts.hpp
+++ engine/modules/concepts.hpp
@@ -1,32 +1,33 @@
 #pragma once
 #include <concepts>
-#include "field/include/field_em.hpp"
+#include "data/field/include/field_em.hpp"
+#include "data/particle/include/particle_system.hpp"
 
+namespace pico::modules
+{
 
-namespace pico::modules {
+    // Field solver concept
+    template <class T, class EMFields, class FieldSystem>
+    concept FieldSolver = requires(T s, EMFields &f, FieldSystem &J, double dt) {
+        { s.solve(f, J, dt) } -> std::same_as<void>;
+    };
 
-// Field solver concept
-template<class T, class EMFields, class FieldSystem>
-concept FieldSolver = requires(T s, EMFields& f, FieldSystem& J, double dt) {
-    { s.solve(f, J, dt) } -> std::same_as<void>;
-};
+    // Particle pusher concept
+    template <class T, class ParticleSystem, class FieldsScratch>
+    concept Pusher = requires(T p, ParticleSystem &part, const FieldsScratch &f, double dt) {
+        { p.push(part, f, dt) } -> std::same_as<void>;
+    };
 
-// Particle pusher concept
-template<class T, class Particles, class Fields>
-concept Pusher = requires(T p, Particles& part, const Fields& f, double dt) {
-    { p.push(part, f, dt) } -> std::same_as<void>;
-};
+    // Collision model concept
+    template <class T, class Particles>
+    concept Collision = requires(T c, Particles &p, double dt) {
+        { c.apply(p, dt) } -> std::same_as<void>;
+    };
 
-// Collision model concept
-template<class T, class Particles>
-concept Collision = requires(T c, Particles& p, double dt) {
-    { c.apply(p, dt) } -> std::same_as<void>;
-};
-
-// Current / charge deposition concept
-template<class T, class Particles, class Fields>
-concept Deposit = requires(T d, const Particles& p, Fields& f) {
-    { d.deposit(p, f) } -> std::same_as<void>;
-};
+    // Current / charge deposition concept
+    template <class T, class Particles, class Fields>
+    concept Deposit = requires(T d, const Particles &p, Fields &f) {
+        { d.deposit(p, f) } -> std::same_as<void>;
+    };
 
 } // namespace pico::modules
