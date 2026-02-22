e4fc23924291f713322a147209569c32e571e5f1
dartrisen <calculating.scientist@gmail.com>
2026-02-22 11:45:34 +0000

    reformat maxwell_yee file

--- kernels/field/maxwell_yee.hpp
+++ kernels/field/maxwell_yee.hpp
@@ -1,36 +1,39 @@
 #pragma once
 
-namespace kernels::field {
+namespace kernels::field
+{
 
-template<size_t BLOCK_SIZE>
-struct MaxwellYeeKernel {
+    template <size_t BLOCK_SIZE>
+    struct MaxwellYeeKernel
+    {
 
-    static inline void update_B(
-        EMFields<BLOCK_SIZE>& fields,
-        const Grid& grid,
-        float dt
-    ) {
-        const float dx_inv = 1.f / grid.cell_size();
+        static inline void update_B(
+            EMFields<BLOCK_SIZE> &fields,
+            const Grid &grid,
+            float dt)
+        {
+            const float dx_inv = 1.f / grid.cell_size();
 
-        // scalar loop version (keep for now)
-        for (size_t i = 0; i < grid.size() - 1; ++i) {
-            fields.B[i] -= dt * dx_inv * (fields.E[i+1] - fields.E[i]);
+            // scalar loop version (keep for now)
+            for (size_t i = 0; i < grid.size() - 1; ++i)
+            {
+                fields.B[i] -= dt * dx_inv * (fields.E[i + 1] - fields.E[i]);
+            }
         }
-    }
 
-    static inline void update_E(
-        EMFields<BLOCK_SIZE>& fields,
-        const FieldSystem<BLOCK_SIZE>& J,
-        const Grid& grid,
-        float dt
-    ) {
-        const float dx_inv = 1.f / grid.cell_size();
+        static inline void update_E(
+            EMFields<BLOCK_SIZE> &fields,
+            const FieldSystem<BLOCK_SIZE> &J,
+            const Grid &grid,
+            float dt)
+        {
+            const float dx_inv = 1.f / grid.cell_size();
 
-        for (size_t i = 1; i < grid.size() - 1; ++i) {
-            fields.E[i] += dt * ((fields.B[i] - fields.B[i-1]) * dx_inv - J[i]);
+            for (size_t i = 1; i < grid.size() - 1; ++i)
+            {
+                fields.E[i] += dt * ((fields.B[i] - fields.B[i - 1]) * dx_inv - J[i]);
+            }
         }
-    }
-};
+    };
 
 } // namespace kernels::field
-
