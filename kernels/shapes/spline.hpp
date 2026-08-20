a606cbeeb1ff945c3fa18aa2e54190a73eb1887d
dartrisen <calculating.scientist@gmail.com>
2026-08-20 14:17:10 +0100

    refactored spline to remove pow function and optimize the throughput

--- kernels/shapes/spline.hpp
+++ kernels/shapes/spline.hpp
@@ -6,26 +6,23 @@
 
 namespace kernels::shapes
 {
-
     template <int Order>
     struct SplineTraits;
 
     template <>
     struct SplineTraits<1>
     {
-        static constexpr int support = 2; // linear / CIC
+        static constexpr int support = 2;
     };
-
     template <>
     struct SplineTraits<2>
     {
-        static constexpr int support = 3; // quadratic / TSC
+        static constexpr int support = 3;
     };
-
     template <>
     struct SplineTraits<3>
     {
-        static constexpr int support = 4; // cubic B-spline
+        static constexpr int support = 4;
     };
 
     template <int Order>
@@ -35,46 +32,41 @@ namespace kernels::shapes
 
         static inline void weights(double x, double dx, int& i0, double (&w)[S])
         {
-            if (dx <= 0.0)
-            {
-                throw std::invalid_argument("dx must be positive");
-            }
-
             const double normalized = x / dx;
             const int    i          = static_cast<int>(std::floor(normalized));
-            double       frac       = normalized - i;
+            const double frac       = normalized - static_cast<double>(i);
 
             i0 = i - (S - 1) / 2;
 
             if constexpr (Order == 1)
             {
-                // linear
                 w[0] = 1.0 - frac;
                 w[1] = frac;
             }
             else if constexpr (Order == 2)
             {
-                // quadratic, 3-point support (TSC-like)
-                const double xm = 1.0 - frac;
-                w[0]            = 0.5 * xm * xm;
-                w[1]            = 0.75 - std::pow(frac - 0.5, 2);
-                w[2]            = 0.5 * frac * frac;
+                const double xm   = 1.0 - frac;
+                const double f_05 = frac - 0.5;
+                w[0]              = 0.5 * xm * xm;
+                w[1]              = 0.75 - (f_05 * f_05);
+                w[2]              = 0.5 * frac * frac;
             }
             else if constexpr (Order == 3)
             {
-                // cubic B-spline
-                const double x0 = frac;
-                const double x1 = 1.0 - frac;
-                w[0]            = (1.0 / 6.0) * std::pow(x1, 3);
-                w[1]            = (1.0 / 6.0) * (4.0 - 6.0 * frac * frac + 3.0 * std::pow(frac, 3));
-                w[2]            = (1.0 / 6.0) * (4.0 - 6.0 * x1 * x1 + 3.0 * std::pow(x1, 3));
-                w[3]            = (1.0 / 6.0) * std::pow(x0, 3);
-            }
-            else
-            {
-                static_assert(Order >= 1 && Order <= 3, "SplineShape supports Order 1..3");
+                constexpr double inv6 = 1.0 / 6.0;
+                const double     x0   = frac;
+                const double     x1   = 1.0 - frac;
+
+                const double x0_2 = x0 * x0;
+                const double x0_3 = x0_2 * x0;
+                const double x1_2 = x1 * x1;
+                const double x1_3 = x1_2 * x1;
+
+                w[0] = inv6 * x1_3;
+                w[1] = inv6 * (4.0 - 6.0 * x0_2 + 3.0 * x0_3);
+                w[2] = inv6 * (4.0 - 6.0 * x1_2 + 3.0 * x1_3);
+                w[3] = inv6 * x0_3;
             }
         }
     };
-
 } // namespace kernels::shapes
