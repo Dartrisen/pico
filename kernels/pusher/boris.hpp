2d72279e5e83c0a7f341cf6838af0d25302e4e52
dartrisen <calculating.scientist@gmail.com>
2026-08-20 09:42:43 +0100

    update boris

--- kernels/pusher/boris.hpp
+++ kernels/pusher/boris.hpp
@@ -15,20 +15,23 @@ namespace kernels::pusher
 
             for (size_t p = 0; p < pb.activeCount; ++p)
             {
+                const float q_over_m  = pb.charge[p] / pb.mass[p];
+                const float half_q_dt = half_dt * q_over_m;
+
                 // --- half electric kick ---
-                float ux = pb.momentum_x[p] + half_dt * fs.Ex[p];
-                float uy = pb.momentum_y[p] + half_dt * fs.Ey[p];
-                float uz = pb.momentum_z[p] + half_dt * fs.Ez[p];
+                float ux = pb.momentum_x[p] + half_q_dt * fs.Ex[p];
+                float uy = pb.momentum_y[p] + half_q_dt * fs.Ey[p];
+                float uz = pb.momentum_z[p] + half_q_dt * fs.Ez[p];
 
                 // --- magnetic rotation ---
-                float tx = half_dt * fs.Bx[p];
-                float ty = half_dt * fs.By[p];
-                float tz = half_dt * fs.Bz[p];
+                float tx = half_q_dt * fs.Bx[p];
+                float ty = half_q_dt * fs.By[p];
+                float tz = half_q_dt * fs.Bz[p];
 
                 float t2 = tx * tx + ty * ty + tz * tz;
-                float sx = 2.f * tx / (1.f + t2);
-                float sy = 2.f * ty / (1.f + t2);
-                float sz = 2.f * tz / (1.f + t2);
+                float sx = 2.0f * tx / (1.0f + t2);
+                float sy = 2.0f * ty / (1.0f + t2);
+                float sz = 2.0f * tz / (1.0f + t2);
 
                 // u' = u- + u- x t
                 float uxp = ux + (uy * tz - uz * ty);
@@ -41,12 +44,13 @@ namespace kernels::pusher
                 uz += (uxp * sy - uyp * sx);
 
                 // --- half electric kick ---
-                pb.momentum_x[p] = ux + half_dt * fs.Ex[p];
-                pb.momentum_y[p] = uy + half_dt * fs.Ey[p];
-                pb.momentum_z[p] = uz + half_dt * fs.Ez[p];
+                pb.momentum_x[p] = ux + half_q_dt * fs.Ex[p];
+                pb.momentum_y[p] = uy + half_q_dt * fs.Ey[p];
+                pb.momentum_z[p] = uz + half_q_dt * fs.Ez[p];
 
-                // --- position update ---
-                pb.position_x[p] += dt * pb.momentum_x[p];
+                // --- position update (v = p / m) ---
+                float vx = pb.momentum_x[p] / pb.mass[p];
+                pb.position_x[p] += dt * vx;
             }
         }
     };
