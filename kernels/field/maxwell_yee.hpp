337dcace87db686cf6ab93b9377513e43a562b38
dartrisen <calculating.scientist@gmail.com>
2026-08-22 23:44:12 +0100

    improve maxwell-yee solver

--- kernels/field/maxwell_yee.hpp
+++ kernels/field/maxwell_yee.hpp
@@ -9,8 +9,7 @@ namespace kernels::field
 template <std::size_t BLOCK_SIZE>
 struct YeeMaxwell
 {
-    static void advance_electric_field(FieldSystem<BLOCK_SIZE>& E, const FieldSystem<BLOCK_SIZE>& B,
-                                       const FieldSystem<BLOCK_SIZE>& J, const Grid& grid, float dt)
+    static void advance_electric_field(FieldSystem<BLOCK_SIZE>& E, const FieldSystem<BLOCK_SIZE>& B, const FieldSystem<BLOCK_SIZE>& J, const Grid& grid, float dt)
     {
         const std::size_t G   = grid.guard_cells();
         const std::size_t N   = grid.physical_size();
@@ -19,17 +18,17 @@ struct YeeMaxwell
         // Loop over physical domain starting at guard cell offset G
         for (std::size_t i = G; i < G + N; ++i)
         {
-            // dBz/dx derivative using staggered grid offset
-            float dBz_dx = (B.field_z(i) - B.field_z(i - 1)) / dxd;
+            // Spatial derivatives of magnetic field components
+            const float dBz_dx = (B.field_z(i) - B.field_z(i - 1)) / dxd;
+            const float dBy_dx = (B.field_y(i) - B.field_y(i - 1)) / dxd;
 
+            E.field_x(i) += -dt * J.field_x(i);
             E.field_y(i) += dt * (-dBz_dx - J.field_y(i));
-            E.field_x(i) += dt * (-J.field_x(i));
-            E.field_z(i) += dt * (-J.field_z(i));
+            E.field_z(i) += dt * (dBy_dx - J.field_z(i));
         }
     }
 
-    static void advance_magnetic_field(FieldSystem<BLOCK_SIZE>& B, const FieldSystem<BLOCK_SIZE>& E, const Grid& grid,
-                                       float dt)
+    static void advance_magnetic_field(FieldSystem<BLOCK_SIZE>& B, const FieldSystem<BLOCK_SIZE>& E, const Grid& grid, float dt)
     {
         const std::size_t G   = grid.guard_cells();
         const std::size_t N   = grid.physical_size();
@@ -37,10 +36,12 @@ struct YeeMaxwell
 
         for (std::size_t i = G; i < G + N; ++i)
         {
-            // dEy/dx derivative
-            float dEy_dx = (E.field_y(i + 1) - E.field_y(i)) / dxd;
+            const float dEy_dx = (E.field_y(i + 1) - E.field_y(i)) / dxd;
+            const float dEz_dx = (E.field_z(i + 1) - E.field_z(i)) / dxd;
 
+            // Advance magnetic field components
             B.field_z(i) -= dt * dEy_dx;
+            B.field_y(i) += dt * dEz_dx;
         }
     }
 };
