6a7ae14ca202ea191146df592f3538d62b63ba1f
dartrisen <calculating.scientist@gmail.com>
2026-08-26 10:17:29 +0100

    update thermal bc

--- engine/modules/boundary/Thermalizing.hpp
+++ engine/modules/boundary/Thermalizing.hpp
@@ -13,7 +13,7 @@ template <std::size_t BLOCK_SIZE>
 class ThermalizingParticleBoundary
 {
 private:
-    float        v_thermal_; // sqrt(k_B * T_wall / m)
+    float        v_thermal_{0.01f}; // sqrt(k_B * T_wall / m)
     std::mt19937 rng_{1337};
 
 public:
@@ -25,29 +25,29 @@ public:
     {
         const double                         L = static_cast<double>(grid.physical_size()) * grid.cell_size();
         std::normal_distribution<float>      gauss(0.0f, v_thermal_);
-        std::exponential_distribution<float> Rayleigh(1.0f / (2.0f * v_thermal_ * v_thermal_));
+        std::exponential_distribution<float> rayleigh(1.0f / (2.0f * v_thermal_ * v_thermal_));
 
         for (std::size_t p = 0; p < block.activeCount; ++p)
         {
-            double x = block.position_x[p];
+            const double x = block.position_x[p];
 
             // Left Wall Collision (x < 0)
             if (x < 0.0)
             {
                 block.position_x[p] = 0.0;
                 // Inject positive momentum x (half-Maxwellian flux)
-                block.momentum_x[p] = block.mass[p] * std::sqrt(Rayleigh(rng_));
-                block.momentum_y[p] = block.mass[p] * gauss(rng_);
-                block.momentum_z[p] = block.mass[p] * gauss(rng_);
+                block.momentum_x[p] = std::sqrt(rayleigh(rng_));
+                block.momentum_y[p] = gauss(rng_);
+                block.momentum_z[p] = gauss(rng_);
             }
             // Right Wall Collision (x >= L)
             else if (x >= L)
             {
                 block.position_x[p] = L - 1e-6;
-                // Inject negative momentum x
-                block.momentum_x[p] = -block.mass[p] * std::sqrt(Rayleigh(rng_));
-                block.momentum_y[p] = block.mass[p] * gauss(rng_);
-                block.momentum_z[p] = block.mass[p] * gauss(rng_);
+                // Inject negative momentum x (half-Maxwellian flux)
+                block.momentum_x[p] = -std::sqrt(rayleigh(rng_));
+                block.momentum_y[p] = gauss(rng_);
+                block.momentum_z[p] = gauss(rng_);
             }
         }
     }
